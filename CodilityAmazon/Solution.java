a62ddb3737b143ab801df7341e23ba1b6a4f2491
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-09-24 11:48:27 +0530

    Codility Amazon Demo Test

---
+++ CodilityAmazon/Solution.java
@@ -0,0 +1,89 @@
+/*
+A zero-indexed array A consisting of N integers is given. An equilibrium index of this array is any integer P such that 0 ≤ P < N and the sum of elements of lower indices is equal to the sum of elements of higher indices, i.e.
+A[0] + A[1] + ... + A[P−1] = A[P+1] + ... + A[N−2] + A[N−1].
+Sum of zero elements is assumed to be equal to 0. This can happen if P = 0 or if P = N−1.
+
+For example, consider the following array A consisting of N = 8 elements:
+
+  A[0] = -1
+  A[1] =  3
+  A[2] = -4
+  A[3] =  5
+  A[4] =  1
+  A[5] = -6
+  A[6] =  2
+  A[7] =  1
+P = 1 is an equilibrium index of this array, because:
+
+A[0] = −1 = A[2] + A[3] + A[4] + A[5] + A[6] + A[7]
+P = 3 is an equilibrium index of this array, because:
+
+A[0] + A[1] + A[2] = −2 = A[4] + A[5] + A[6] + A[7]
+P = 7 is also an equilibrium index, because:
+
+A[0] + A[1] + A[2] + A[3] + A[4] + A[5] + A[6] = 0
+and there are no elements with indices greater than 7.
+
+P = 8 is not an equilibrium index, because it does not fulfill the condition 0 ≤ P < N.
+
+Write a function:
+
+class Solution { public int solution(int[] A); }
+
+that, given a zero-indexed array A consisting of N integers, returns any of its equilibrium indices. The function should return −1 if no equilibrium index exists.
+
+For example, given array A shown above, the function may return 1, 3 or 7, as explained above.
+
+Assume that:
+
+N is an integer within the range [0..100,000];
+each element of array A is an integer within the range [−2,147,483,648..2,147,483,647].
+Complexity:
+
+expected worst-case time complexity is O(N);
+expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+*/
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		int [] A = {-1, 3, -4, 5, 1, -6, 2, 1};
+		System.out.println(solution(A));
+	}
+
+	public static int solution(int[] A) {
+
+        int [] sums = new int[A.length];
+        sums[0] = A[0];
+        for (int i = 1; i < A.length; ++i) {
+        	sums[i] = A[i] + sums[i-1];
+        }
+
+        int total = sums[A.length-1];
+        int leftSum = 0;
+        int rightSum = 0;
+
+        for (int i = 0; i < A.length; ++i) {
+        	leftSum = 0;
+        	rightSum = 0;
+        	if (i == 0) {
+        		if (total-A[i] == 0) {
+        			return 0;
+        		}
+        	} else {
+        		if ((total - A[i]) % 2 != 1) {
+        			leftSum = sums[i-1];
+        			rightSum = total-A[i]-leftSum;
+        			if (leftSum == rightSum)
+        				return i;
+        		}
+        	}
+        }
+
+        return -1;
+    }
+}
+
+//-1 3 -4 5 1 -6 2 1
+//-1 2 -2 3 4 -2 0 1

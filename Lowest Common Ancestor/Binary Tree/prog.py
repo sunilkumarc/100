5912a86dc2ea21b7c325e3850e7ddb4b49df62e3
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-10-21 19:34:22 +0530

    Finding lowest common ancestor in a binary tree
    Without using vector array:
    1. Assuming both the keys exist in the tree
    2. No assumptions - proper algorithm

--- Lowest Common Ancestor/Binary Tree/prog.py
+++ Lowest Common Ancestor/Binary Tree/prog.py
@@ -7,6 +7,8 @@ class Node():
 class BinaryTree():
     def __init__(self):
         self.root = None
+        self.n1Present = False
+        self.n2Present = False
 
     def insert(self, arr):
         self.root = Node(arr[0])
@@ -50,7 +52,7 @@ class BinaryTree():
         path.pop()
         return path, False
 
-    def lowestCommonAncestor(self, n1, n2):
+    def lowestCommonAncestor1(self, n1, n2):
         path1 = []
         path2 = []
         path1, n1Present = self.findPath(self.root, path1, n1)
@@ -66,6 +68,65 @@ class BinaryTree():
 
         return path1[min(len(path1), len(path2))-1]
 
+    def lowestCommonAncestor2Utility(self, root, n1, n2):
+        if root == None:
+            return None
+
+        if root.key == n1 or root.key == n2:
+            return root.key
+
+        leftLCA = self.lowestCommonAncestor2Utility(root.left, n1, n2)
+        rightLCA = self.lowestCommonAncestor2Utility(root.right, n1, n2)
+
+        if leftLCA != None and rightLCA != None:
+            return root.key
+
+        if leftLCA != None:
+            return leftLCA
+        return rightLCA
+
+    def lowestCommonAncestor2(self, n1, n2):
+        return self.lowestCommonAncestor2Utility(self.root, n1, n2)
+
+    def lowestCommonAncestor3Utility(self, root, n1, n2):
+        if root == None:
+            return None
+
+        if root.key == n1:
+            self.n1Present = True
+            return root
+        if root.key == n2:
+            self.n2Present = True
+            return root
+
+        leftLCA = self.lowestCommonAncestor3Utility(root.left, n1, n2)
+        rightLCA = self.lowestCommonAncestor3Utility(root.right, n1, n2)
+
+        if leftLCA != None and rightLCA != None:
+            return root
+
+        if leftLCA != None:
+            return leftLCA
+
+        return rightLCA
+
+    def findTheOther(self, root, n):
+        if root == None:
+            return False
+
+        if (root.key == n) or (self.findTheOther(root.left, n)) or (self.findTheOther(root.right, n)):
+            return True
+
+        return False
+
+    def lowestCommonAncestor3(self, n1, n2):
+        n1Present = False
+        n2Present = False
+
+        lca = self.lowestCommonAncestor3Utility(self.root, n1, n2)
+        if ((self.n1Present and self.n2Present) or (self.n1Present and self.findTheOther(lca, n2)) or (self.n2Present and self.findTheOther(lca, n1))):
+            return lca.key
+        return None
 
 if __name__ == '__main__':
     bt = BinaryTree()
@@ -77,7 +138,16 @@ if __name__ == '__main__':
     #     2       3
     # 4       5 6     7
     #         8
+
     # First way
     # Create two arrays with paths from root to n1 and root to n2
     # Then traverse through both the arrays until you find first non equal element and return the previous element
-    print('Lowest Common Ancestor - method1 (5, 6)', bt.lowestCommonAncestor(6, 8))
+    print('Lowest Common Ancestor using Vector Array - ', bt.lowestCommonAncestor1(4, 7))
+
+    # Second way
+    # Assuming both the elements are present in the tree
+    print('Lowest Common Ancestor assuming both keys exist - ', bt.lowestCommonAncestor2(4, 7))
+
+    # Third wasy
+    # Similar to second one. But doesn't assume both the keys are present in the tree
+    print('Lowest Common Ancestor with no assumptions - ', bt.lowestCommonAncestor3(4, 7))

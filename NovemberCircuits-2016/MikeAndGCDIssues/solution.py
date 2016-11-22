27807f2056ab05592fbbc503e351e16e8d750888
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-11-22 23:32:51 +0530

    8/35 passing now

--- NovemberCircuits-2016/MikeAndGCDIssues/solution.py
+++ NovemberCircuits-2016/MikeAndGCDIssues/solution.py
@@ -1,33 +1,71 @@
 def gcd(a, b): return a if b == 0 else gcd(b, a%b)
 # def lcm(a, b): return a / gcd(a, b) * b
+# 2 3 4 9 17
 
 def findJ(arr, N):
     result = []
+    store = dict()
+
+    def exists(left, right):
+        if str(arr[left])+'+'+str(arr[right]) in store:
+            return True
+        return False
+
+    def getGcd(left, right):
+        return store[str(arr[left])+'+'+str(arr[right])]
+
+    def insert(left, right, res):
+        store[str(arr[left])+'+'+str(arr[right])] = res
+
     for i in range(N):
         found = False
         left = i - 1
         right = i + 1
         js = []
-        while left >= 0 and right < N:
-            js.append(left)
-            js.append(right)
-            left -= 1
-            right += 1
-
-        while left >= 0:
-            js.append(left)
-            left -= 1
-
-        while right < N:
-            js.append(right)
-            right += 1
-
-        for j in js:
-            res = gcd(arr[i], arr[j])
+        while found == False and left >= 0 and right < N:
+            if exists(left, i):
+                res = getGcd(left, i)
+            else:
+                res = gcd(arr[left], arr[i])
+                insert(left, i, res)
             if res > 1:
-                result.append(j+1)
+                result.append(left+1)
                 found = True
-                break
+
+            if found == False:
+                if exists(i, right):
+                    res = getGcd(i, right)
+                else:
+                    res = gcd(arr[i], arr[right])
+                    insert(i, right, res)
+                if res > 1:
+                    result.append(right+1)
+                    found = True
+
+            left -= 1
+            right += 1
+
+        while found == False and left >= 0:
+            if exists(left, i):
+                res = getGcd(left, i)
+            else:
+                res = gcd(arr[left], arr[i])
+                insert(left, i, res)
+            if res > 1:
+                result.append(left+1)
+                found = True
+            left -= 1
+
+        while found == False and right < N:
+            if exists(i, right):
+                res = getGcd(i, right)
+            else:
+                res = gcd(arr[i], arr[right])
+                insert(i, right, res)
+            if res > 1:
+                result.append(right+1)
+                found = True
+            right += 1
 
         if found == False:
             result.append(-1)

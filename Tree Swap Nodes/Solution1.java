80ac04d2494dcd7862154a73e0bc29f0e4bc3a45
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-12-01 22:43:16 +0530

    Tree construction - done

---
+++ Tree Swap Nodes/Solution1.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+class Tree {
+	List<Node> nodes = new ArrayList<>();
+
+	Tree(int N) {
+		for (int i = 0; i <= N; ++i) {
+			Node node = new Node(i);
+			nodes.add(node);
+		}
+	}
+
+	void insert(int index, int l, int r) {
+		if (l == -1)
+			nodes.get(index).left = null;
+		else
+			nodes.get(index).left = nodes.get(l);
+
+		if (r == -1)
+			nodes.get(index).right = null;
+		else
+			nodes.get(index).right = nodes.get(r);
+	}
+
+	void inorderTraversal(Node root) {
+		if (root != null) {
+			inorderTraversal(root.left);
+			System.out.println(root.data);
+			inorderTraversal(root.right);
+		}
+	}
+
+	void inorder() {
+		this.inorderTraversal(this.nodes.get(1));
+	}
+}
+
+public class Solution1 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine().trim());
+		String [] line;
+		int left, right;
+
+		if (N > 0) {
+			Tree tree = new Tree(N);
+
+			for (int index = 1; index <= N; ++index) {
+				line = br.readLine().trim().split(" ");
+				left = Integer.parseInt(line[0]);
+				right = Integer.parseInt(line[1]);
+
+				tree.insert(index, left, right);
+			}
+
+			tree.inorder();
+		}
+	}
+}

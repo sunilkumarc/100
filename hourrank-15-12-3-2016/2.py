8df98c3b207c792c163ace3dd5baeb2d13609054
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-12-11 22:31:04 +0530

    Incomplete - 17/20 done!

--- hourrank-15-12-3-2016/2.py
+++ hourrank-15-12-3-2016/2.py
@@ -1,43 +1,29 @@
+# https://www.hackerrank.com/contests/hourrank-15/challenges/an-interesting-game-1/
+
+import collections
 Q = int(input().strip())
 
-maxArray = []
-maxIndex = 0
-
-def initMaxArrayAndIndex(arr):
-    global maxArray
-
-    for i in range(len(arr)):
-        print(arr[i])
-        maxArray[arr[i]] = i
-
-def findMax(arr):
-    global maxArray
-    global maxIndex
-
-    while maxIndex >= 0:
-        if maxArray[maxIndex] != -1 and len(arr) > maxArray[maxIndex]:
+def findMax(arr, sortedMap):
+    while True:
+        key, value = sortedMap.popitem()
+        if value < len(arr):
             break
-        maxIndex -= 1
-
-    res = maxArray[maxIndex]
-    maxIndex -= 1
-    return res
+    return value, sortedMap
 
 for _ in range(Q):
     N = int(input().strip())
     arr = list(map(int, input().strip().split()))
 
-    # global maxArray
-    # global maxIndex
+    myMap = {}
+    for i in range(len(arr)):
+        myMap[arr[i]] = i
 
-    maxArray = [-1 for _ in range(1000000001)]
-    maxIndex = 1000000000
-
-    initMaxArrayAndIndex(arr)
+    sortedMap = collections.OrderedDict(sorted(myMap.items()))
     bobsTurn = 1
     while len(arr) > 0:
-        maxElem = findMax(arr)
-        arr = arr[:maxElem]
+        print(sortedMap)
+        maxElemIndex, sortedMap = findMax(arr, sortedMap)
+        arr = arr[:maxElemIndex]
         bobsTurn *= -1
 
     if bobsTurn == 1:

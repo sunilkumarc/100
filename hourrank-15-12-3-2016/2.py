6ac1dddf2827ec4848df45fc19165bd00c296c0b
Sunil Kumar C <sunilkumar.c682@gmail.com>
2016-12-14 22:51:19 +0530

    Gaming array - Completed!

--- hourrank-15-12-3-2016/2.py
+++ hourrank-15-12-3-2016/2.py
@@ -1,32 +1,19 @@
 # https://www.hackerrank.com/contests/hourrank-15/challenges/an-interesting-game-1/
 
-import collections
 Q = int(input().strip())
-
-def findMax(arr, sortedMap):
-    while True:
-        key, value = sortedMap.popitem()
-        if value < len(arr):
-            break
-    return value, sortedMap
 
 for _ in range(Q):
     N = int(input().strip())
     arr = list(map(int, input().strip().split()))
 
-    myMap = {}
-    for i in range(len(arr)):
-        myMap[arr[i]] = i
+    count = 1
+    currMax = arr[0]
+    for i in range(1, N):
+        if arr[i] > currMax:
+            count += 1
+            currMax = arr[i]
 
-    sortedMap = collections.OrderedDict(sorted(myMap.items()))
-    bobsTurn = 1
-    while len(arr) > 0:
-        print(sortedMap)
-        maxElemIndex, sortedMap = findMax(arr, sortedMap)
-        arr = arr[:maxElemIndex]
-        bobsTurn *= -1
-
-    if bobsTurn == 1:
+    if count % 2 == 0:
         print('ANDY')
     else:
         print('BOB')

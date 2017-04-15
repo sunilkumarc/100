fc376abfce33a6a1a28bc47efe464b5e1d56e2c3
Sunil Kumar C <sunilkumar.c682@gmail.com>
2017-04-15 22:32:48 +0530

    Started with 'Introduction to Algorithms'

---
+++ Insertion Sort/prog.py
@@ -0,0 +1,12 @@
+a = [5, 4, 2, 6, 1, 8, 1]
+print(a)
+
+for i in range(1, len(a)):
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > key:
+        a[j+1] = a[j]
+        j = j -1
+    a[j+1] = key
+
+print(a)

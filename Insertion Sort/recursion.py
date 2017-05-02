967078edcb2fcbcd7319eac9c4be84912c174cb4
Sunil Kumar C <sunilkumar.c682@gmail.com>
2017-05-02 23:59:10 +0530

    Recursive implementation of insertion sort

---
+++ Insertion Sort/recursion.py
@@ -0,0 +1,16 @@
+a = [3, 8, 0, 1, 9, 6, -7]
+
+def insertionSort(arr, start, end):
+    if end > 0:
+        arr = insertionSort(arr, 0, end-1)
+    
+    key = arr[end]
+    j = end-1
+    while j >= 0 and arr[j] > key:
+        a[j+1] = a[j]
+        j = j -1
+    a[j+1] = key
+    return arr
+
+arr = insertionSort(a, 0, len(a)-1)
+print(arr)

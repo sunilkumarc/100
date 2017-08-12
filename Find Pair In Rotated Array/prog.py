6bb09dc7d1e257f81cc906898247c8545dcd6952
Sunil Kumar C <sunilkumar.c682@gmail.com>
2017-08-12 15:51:28 +0530

    Finding pair with a given sum in a rotated array

---
+++ Find Pair In Rotated Array/prog.py
@@ -0,0 +1,48 @@
+# http://www.geeksforgeeks.org/given-a-sorted-and-rotated-array-find-if-there-is-a-pair-with-a-given-sum/
+
+def findPivot(arr):
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            return i
+    
+    return -1
+
+def findPair(arr, x):
+    pivot = findPivot(arr)
+    if pivot != -1:
+        start = pivot + 1
+        end = pivot
+        while start < len(arr) and end >= 0:
+            currentSum = arr[start] + arr[end]
+            if currentSum == x:
+                return [start, end]
+            elif currentSum < x:
+                start += 1
+            else:
+                end -= 1
+    else:
+        start = 0
+        end = len(arr) - 1
+        while start < end:
+            currentSum = arr[start] + arr[end]
+            if currentSum == x:
+                return [start, end]
+            elif currentSum < x:
+                start += 1
+            else:
+                end -= 1
+        
+    return None
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+    x = int(input().strip())
+
+    print('Array : ', arr)
+    print('Sum : ', x)
+    result = findPair(arr, x)
+    if result != None:
+        print('Found a pair : ', arr[result[0]], arr[result[1]])
+    else:
+        print('No such pair found!')
